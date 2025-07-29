8176acb5b38b7912192dbe05739d5304c248badd
Melanie Melo <141352813+melaniemelo@users.noreply.github.com>
2025-07-29 15:30:05 -0500

    Update trunc_euk_widom_protein_rg.sh

--- analysis_scripts/Figure5-Protein_Rg_Hbond/radius_of_gyration/trunc_euk_widom_protein_rg.sh
+++ analysis_scripts/Figure5-Protein_Rg_Hbond/radius_of_gyration/trunc_euk_widom_protein_rg.sh
@@ -14,11 +14,11 @@ do
 
 
 cpptraj <<_EOF
-parm trunc_euk_widom.run$i.prmtop
+parm trunc_euk_widom.prmtop
 trajin trunc_euk_widom.run$i.xtc
 autoimage
 align :301-392,402-474,475-557,585-672,680-771,781-853,854-936,964-1051&!@H= first
-radgyr Run1 :301-392,402-474,475-557,585-672,680-771,781-853,854-936,964-1051&!@H= out trunc_euk_widom_protein_rg$i.dat nomax mass
+radgyr Run$i :301-392,402-474,475-557,585-672,680-771,781-853,854-936,964-1051&!@H= out trunc_euk_widom_protein_rg$i.dat nomax mass
 go
 clear all
 
