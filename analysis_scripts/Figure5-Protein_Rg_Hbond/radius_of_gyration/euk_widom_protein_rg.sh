9d5cad4d8d30f6fac4a9d6e75450cff96c68763d
Melanie Melo <141352813+melaniemelo@users.noreply.github.com>
2025-07-29 15:53:44 -0500

    Update euk_widom_protein_rg.sh

--- analysis_scripts/Figure5-Protein_Rg_Hbond/radius_of_gyration/euk_widom_protein_rg.sh
+++ analysis_scripts/Figure5-Protein_Rg_Hbond/radius_of_gyration/euk_widom_protein_rg.sh
@@ -19,7 +19,7 @@ parm euk_widom.prmtop
 trajin euk_widom.run$i.xtc
 autoimage
 align :338-429,459-531,547-629,693-780,825-916,946-1018,1034-1116,1180-1267&!@H= first
-radgyr Run$i :338-429,459-531,547-629,693-780,825-916,946-1018,1034-1116,1180-1267&!@H= out euk_widom_protein_rg$i.dat nomax mass
+radgyr Rg :338-429,459-531,547-629,693-780,825-916,946-1018,1034-1116,1180-1267&!@H= out euk_widom_protein_rg$i.dat nomax mass
 go
 clear all
 
